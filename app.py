7a979ac53fc37441e9390d65af82c1a33d3ef0f9
bibliotekue <semenpuchin@yandex.com>
2022-01-19 17:12:17 +0500

    added analyzer and re-architectured main app

--- app.py
+++ app.py
@@ -1,31 +1,11 @@
 import json
 import requests
 import tinvest
-from collections import defaultdict
 import pandas as pd
 from secrets import TOKEN
 
-import stats_load
-
-# preparation
-INDEX_NAME = 'nasdaq100'
-stats = stats_load.url_parse(INDEX_NAME)
-portfolio_df = stats_load.stats_load(stats)
-
-
-# get tickers info {ticker: weight}
-tickers_info = defaultdict()
-for elem in portfolio_df.itertuples():
-    ticker = elem[3]
-    weight = elem[4]
-
-    tickers_info[ticker] = weight
-
-
-# get list of tickers
-TICKERS = list()
-for k in tickers_info.keys():
-    TICKERS.append(k)
+import webscrapper
+import analyzer
 
 
 # get client instance
@@ -35,44 +15,21 @@ client = tinvest.SyncClient(TOKEN)
 portfolio = client.get_portfolio()
 currencies = client.get_portfolio_currencies()
 
+web_data = webscrapper.extract_web_data(index=webscrapper.INDEXES['NASDAQ'])
+portfolio_data = analyzer.extract_portfolio_data(portfolio, web_data['Symbol'].values)
 
-# currencies parser
-CURRENCY = 'usd'
-CURRENCY_BALANCE = 0
-for currency in currencies.payload.currencies:
-    name = currency.currency.name
-    balance = currency.balance
+# get balances
+usd_balance = currencies.payload.currencies[2].balance
+total_investments = sum(portfolio_data['asset_investments'])
 
-    CURRENCY_BALANCE += balance if name.lower() in CURRENCY else 0
-
-
-# get assets from portfolio that consists in index
-my_columns = ['asset', 'amount', 'price', 'weight', 'asset_investments']
-df = pd.DataFrame(columns=my_columns)
-
-for position in portfolio.payload.positions:
-    ticker = position.ticker
-
-    if ticker not in 'USD000UTSTOM' and ticker in TICKERS:
-        amount = position.lots
-        price = position.average_position_price.value
-
-        asset_investments = price * amount
-
-        df = df.append(pd.DataFrame([[ticker,
-                                      amount,
-                                      price,
-                                      0,
-                                      asset_investments]],
-                                    columns=my_columns))
-
-# get total investments
-TOTAL_INVESTMENTS = sum(df['asset_investments']) + CURRENCY_BALANCE
+total_balance = total_investments + usd_balance
 
 # weight calculation
-for elem in df.itertuples():
+for elem in portfolio_data.itertuples():
     ticker = elem[1]
     asset_investments = elem[5]
-    weight = asset_investments / TOTAL_INVESTMENTS
-    df.loc[df['asset'] == ticker, 'weight'] = weight
+    weight = asset_investments / total_balance
+    portfolio_data.loc[portfolio_data['asset'] == ticker, 'weight'] = weight
+
+print(portfolio_data)
 
