4a172d0fe4761d005c892ba8731b00350eb93418
bibliotekue <semenpuchin@yandex.com>
2022-01-19 17:13:23 +0500

    re-architectured webscrapper

--- webscrapper.py
+++ webscrapper.py
@@ -11,17 +11,19 @@ URL_INFO = {
     'ATTRS': 'table table-hover table-borderless table-sm'
             }
 
+INDEXES = {'SP500': 'sp500',
+           'NASDAQ': 'nasdaq100',
+           'DOWJONES': 'dowjones'}
+
 COLUMNS = ['#', 'Company', 'Symbol', 'Weight', 'Price']
 
 
-def url_parse(index_name):
-    response = requests.get(URL_INFO['URL'] + index_name, headers=URL_INFO['HEADER'])
+def extract_web_data(index):
+    response = requests.get(URL_INFO['URL'] + index, headers=URL_INFO['HEADER'])
 
     soup = BeautifulSoup(response.text, URL_INFO['PARSER'])
-    stats = soup.find(URL_INFO['TAG'], class_=URL_INFO['ATTRS'])
-    return stats
+    index_info = soup.find(URL_INFO['TAG'], class_=URL_INFO['ATTRS'])
 
-
-def stats_load(stats):
-    df = pd.read_html(str(stats))[0]
+    df = pd.read_html(str(index_info))[0]
     return df[COLUMNS]
+
