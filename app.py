d384a1399d42c7f07bfd98ac7a5c9752fc9d2d17
bibliotekue <semenpuchin@yandex.com>
2022-01-20 12:23:37 +0500

    added config

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-import json
-import requests
 import tinvest
-import pandas as pd
-from secrets import TOKEN, CONFIG_PATH
 
+from secrets import TOKEN, CONFIG_PATH
 from extractor import Extractor
 
 # get client instance
 client = tinvest.SyncClient(TOKEN)
 
-
 surfer = Extractor(client_instance=client,
                    config_path=CONFIG_PATH)
-
 surfer.run()
 
 print(surfer.portfolio_data)
