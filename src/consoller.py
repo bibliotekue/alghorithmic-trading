4da7d5a19f9e920c2bf5c27419a0e1396169cc8f
bibliotekue <semenpuchin@yandex.com>
2022-01-25 14:39:27 +0500

    added description comments to consoller

--- src/consoller.py
+++ src/consoller.py
@@ -1,11 +1,14 @@
 import argparse
 
+# create parser instance
 parser = argparse.ArgumentParser(description='This program is analysis your Tinkoff.Invest portfolio and returned '
                                              'a list of stocks and their amount to mirror chosen Index. ')
 
+# add argument to console command which represents Index name for mirroring
 parser.add_argument('-i',
                     "--index",
                     help="Index which you want to mirror. There is only 3 indexes such as: SP500, NASDAQ and DOWJONES.",
                     default='SP500')
 
+# argparse variable
 args = parser.parse_args()
